eb4f2285c7b73f1013cf1e161d82c6877490839e
AlexanderM1927 <82120515+AlexanderMGB@users.noreply.github.com>
2021-08-18 11:10:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+To run this project, you should installl dependencies, using:
+
+npm install
+
+Then should install quasar from quasar/cli, can be globally using this command:
+
+npm install -g @quasar/cli+
+
+after that to run into dev mode, you should run:
+
+quasar dev
+
+And that's it.
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <!-- <router-view /> -->
+
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    // EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
