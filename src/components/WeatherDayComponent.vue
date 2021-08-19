56c3e846fe74869d3471904945822ae116b20ade
AlexanderM1927 <82120515+AlexanderMGB@users.noreply.github.com>
2021-08-19 17:02:12 -0500

    fix into weather next days

--- src/components/WeatherDayComponent.vue
+++ src/components/WeatherDayComponent.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="my-day">
-    Hola
+    <div class="row">
+      <div class="col-8">
+        <div class="my-padding-10">
+          <div class="my-img-description">
+            <img :src="'http://openweathermap.org/img/w/04d.png'">
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="my-weather">
+          32°/23°
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +33,16 @@ export default defineComponent({
   margin: 5px;
   border: 1px solid #eaeaea;
   border-radius: 10px;
-  padding: 5px;
+  // padding: 10px;
+}
+.my-img-description {}
+.my-weather {
+  font-size: 25px;
+  padding: 10px;
+  background: #7c67ee;
+  height: 100%;
+  border-radius: 10px;
+  color: white;
+  font-weight: bold;
 }
 </style>
