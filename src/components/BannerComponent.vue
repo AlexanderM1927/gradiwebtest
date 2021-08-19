738ba9120f40311e0cf19dd5aac397e20f504a74
AlexanderM1927 <82120515+AlexanderMGB@users.noreply.github.com>
2021-08-19 10:17:48 -0500

    Logo + main container.

---
+++ src/components/BannerComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="my-banner">
+    <div class="my-city-name"><q-icon name="place"/>{{text}}</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BannerComponent',
+  data () {
+    return {
+      text: 'Bogota'
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.my-banner {
+  border-radius: 20px;
+  background: url('../assets/bg-banner.png') bottom #a1c8d0;
+  background-position-x: 100%;
+  background-position-y: 5%;
+  background-size: 100%;
+  background-position: fixed;
+  background-repeat: no-repeat;
+  color: #8d95f2;
+  font-size: 20px;
+  font-weight: bold;
+  height: 300px;
+  padding: 4.2% 4%;
+}
+.my-city-name {
+  vertical-align: middle;
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,21 +1,32 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout class="my-layout" view="lHh Lpr lFf">
     <q-page-container>
       <!-- <router-view /> -->
-      Hello world.<br>
-      {{text}}
+      <div class="row">
+        <div class="col-md-11 col-xs-9"></div>
+        <div class="col-md-1 col-xs-3">
+          <center>
+            <img src="../assets/logo.png">
+          </center>
+        </div>
+      </div>
+      <div class="my-main-box">
+        <banner-component></banner-component>
+      </div>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import BannerComponent from '../components/BannerComponent'
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
     // EssentialLink
+    BannerComponent
   },
 
   // setup () {
@@ -37,7 +48,16 @@ export default defineComponent({
 })
 </script>
 <style lang="scss" scoped>
-body {
+
+.my-layout {
   font-family: Arial, Helvetica, sans-serif;
+  background: linear-gradient(to right, #dce3eb 30%, #d3d9e0 50%);
+  height: 100%;
+}
+.my-main-box {
+  background: white;
+  padding: 2%;
+  margin: 1% 4%;
+  border-radius: 20px;
 }
 </style>
